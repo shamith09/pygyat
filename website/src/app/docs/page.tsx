ddbe9bbcb305c65df1e91986e4d2231550587fde
Shamith Pasula <shamith09@berkeley.edu>
2024-12-26 00:23:52 -0800

    update BMC

--- website/src/app/docs/page.tsx
+++ website/src/app/docs/page.tsx
@@ -64,7 +64,7 @@ export default function Documentation() {
         data-name="BMC-Widget"
         data-cfasync="false"
         src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-        data-id="support.pygyat"
+        data-id="shamith"
         data-description="Support me on Buy me a coffee!"
         data-message=""
         data-color="#BD5FFF"
